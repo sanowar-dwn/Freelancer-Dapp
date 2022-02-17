89547fd1c57ea3128600f59a99e0a718730ab039
Identity Office Dev <sanowardwnofficial@gmail.com>
2022-02-17 13:43:36 +0600

    dash board changes

--- resources/views/admin/profile/profile.blade.php
+++ resources/views/admin/profile/profile.blade.php
@@ -18,4 +18,29 @@
         </div>
     </div>
 </div>
+<div class="col-lg-4">
+    <div class="card">
+        <div class="card-header">Update Password</div>
+        <div class="card-body">
+            <form action="{{ url('/profile/name_change') }}" method="POST" class="form-horizontal">
+                @csrf
+                <div class="mt-2">
+                    <label for="">Old Password</label>
+                    <input type="password" name="old_password" class="form-control">
+                </div>
+                <div class="mt-2">
+                    <label for="">New Password</label>
+                    <input type="text" name="new_password" class="form-control">
+                </div>
+                <div class="mt-2">
+                    <label for="">Confirm new password</label>
+                    <input type="text" name="confirm_password" class="form-control">
+                </div>
+                <div class="mt-2">
+                    <button class="btn btn-success" type="submit">UPDATE</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
